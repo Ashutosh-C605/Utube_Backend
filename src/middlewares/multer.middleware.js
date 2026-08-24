6e29a78608d74d5f81fb7dd4cd20b8dda9e512ca
Ashutosh Chaturvedi <ashutoshchat605@gmail.com>
2026-08-25 03:20:55 +0530

    Basic setup done

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp"); // Specify the destination folder for uploaded files   
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round
+        (Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix); // Generate a unique filename for the uploaded file
+    }
+})
+
+const upload = multer({ storage: storage });
+
+export const upload = multer({ storage, });
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary" //image and video hosting service
+import { response } from "express"
+import fs from "fs" //file system module
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file to cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded to cloudinary
+        console.log("File uploaded to cloudinary successfully", 
+            response.url)
+            return  response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //delete the file from local storage
+        console.error("Error uploading file to cloudinary", error)
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
